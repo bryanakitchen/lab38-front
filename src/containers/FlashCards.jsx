6ddd538a5177d5cd5c6a8cd2dfba6c0111f0b576
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-22 19:29:19 -0500

    adds flashcards container list - red

---
+++ src/containers/FlashCards.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FlashCards() {
+  return (
+    <div>
+        hello
+    </div>
+  );
+}
+
+FlashCards.propTypes = {
+
+};
+
+export default FlashCards;
---
+++ src/containers/FlashCards.test.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import FlashCards from './FlashCards';
+
+const server = setupServer(
+  rest.get('https://cryptic-waters-90298.herokuapp.com/api/v1/cards', (req, res, ctx) => {
+    return res(ctx.json([
+      { 
+        id: '123',
+        keyTerm: 'HTTP',
+        definition: 'Hypertext Transfer Protocol',
+        topic: 'web'
+      },
+      { 
+        id: '456',
+        keyTerm: 'CORS', 
+        definition: 'Cross-Origin Resource Sharing', 
+        topic: 'JavaScript'
+      },
+      { 
+        id: '789',
+        keyTerm: 'DOM', 
+        definition: 'Document Object Model', 
+        topic: 'JavaScript' 
+      },
+    ]));
+  })
+);
+
+describe('FlashCards component', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('renders a list of flashcards', () => {
+    render(<FlashCards />);
+
+    return waitFor(() => {
+      screen.getByText('Hypertext Transfer Protocol', { exact: false });
+      screen.getByText('Cross-Origin Resource Sharing', { exact: false });
+      screen.getByText('Document Object Model', { exact: false });
+    });
+  });
+});
